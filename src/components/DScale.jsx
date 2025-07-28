2e7f8efeda3b334a3eec034c6aa3450d5274a9ae
justinbornais <justin.bornais@gmail.com>
2025-07-28 14:07:39 -0400

    Added D major scale

---
+++ src/components/DScale.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useRef, useState } from 'react';
+import {
+  Accidental,
+  Annotation,
+  Barline,
+  Renderer,
+  Stave,
+  StaveNote,
+  Voice,
+  Formatter,
+  Beam,
+} from 'vexflow';
+
+export default function CScale() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const width = 960;
+    const div = containerRef.current;
+    if (!div) return;
+  
+    div.innerHTML = '';
+  
+    const renderer = new Renderer(div, Renderer.Backends.SVG);
+    renderer.resize(width, 100);
+    const context = renderer.getContext();
+    context.setFont('Arial', 10, '').setBackgroundFillStyle('#fff');
+  
+    const stave = new Stave(0, 0, width - 4);
+    stave.addClef('treble');
+    stave.setEndBarType(Barline.type.END);
+    stave.addKeySignature('D');
+    stave.setContext(context).draw();
+  
+    const ascending = ['d/4', 'e/4', 'f#/4', 'g/4', 'a/4', 'b/4', 'c#/5', 'd/5'];
+    const descending = [...ascending].reverse();
+  
+    const notes = [...ascending, ...descending].map((note, i) => {
+      const sn = new StaveNote({ keys: [note], duration: '8' });
+      const fingering = new Annotation("1")
+        .setFont("Arial", 12)
+        .setVerticalJustification(Annotation.VerticalJustify.TOP);
+      sn.addModifier(fingering, 0);
+      if (note.includes('#'))
+        sn.addModifier(new Accidental('#'), 0);
+      return sn;
+    });
+  
+    const voice = new Voice({ num_beats: 16, beat_value: 8 });
+    voice.addTickables(notes);
+  
+    const beams = Beam.generateBeams(notes);
+  
+    new Formatter().joinVoices([voice]).format([voice], width - 50);
+    voice.draw(context, stave);
+    beams.forEach(beam => beam.setContext(context).draw());
+  }, []);
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>D Major</h2>
+      <div ref={containerRef} style={{
+        maxWidth: '960px',
+        margin: '0 auto', // Horizontal centering.
+      }} />
+    </div>
+  );
+}
